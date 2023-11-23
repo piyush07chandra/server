22d25454bee22cd2201b7db103a46f7c961fd9f3
piyush <piyushchandra9458@gmail.com>
2023-11-23 03:19:13 -1200

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+import express from 'express'
+
+const main =async()=>{
+    try {
+        await mongoose.connect(`mongodb+srv://piyush:LzXIr0Z8D3Vje19c@web-ecomerce.3wphvcu.mongodb.net/?retryWrites=true&w=majority`)
+        console.log("database connected")
+     }
+     catch (error){
+        console.log("error while connecting with the database",error.message);
+     }
+}
+main()
+
+const app=express()
+const port=3000
+
+app.get('/',(req,res)=>{
+   res.send("ok connected")
+})
+app.listen(port ,()=>console.log("server is running on ",port))
