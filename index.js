8531a5aa3f2756e3c55b3cf2b3687860b764d3dd
piyush <piyushchandra9458@gmail.com>
2023-11-24 20:35:21 -1200

    okk

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ const app=express()
 
 
 
+
 const main =async()=>{
+   const URL=`mongodb+srv://piyush:LzXIr0Z8D3Vje19c@web-ecomerce.3wphvcu.mongodb.net/?retryWrites=true&w=majority`;
     try {
-        await mongoose.connect(`mongodb://127.0.0.1:27017/heliverse`, { 
+        await mongoose.connect((URL), { 
          serverSelectionTimeoutMS: 5000 // Set to a higher value if needed
      })
         console.log("database connected")
