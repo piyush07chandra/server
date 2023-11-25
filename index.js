11463c44c8b9a0d77d9e6f30f9416d1fe0f36a47
piyush <piyushchandra9458@gmail.com>
2023-11-24 21:29:46 -1200

    ok

--- index.js
+++ index.js
@@ -10,9 +10,11 @@ const app=express()
 
 
 const main =async()=>{
-   const URL=`mongodb+srv://piyush:LzXIr0Z8D3Vje19c@web-ecomerce.3wphvcu.mongodb.net/?retryWrites=true&w=majority`;
+   // const URL=`mongodb+srv://piyush:LzXIr0Z8D3Vje19c@web-ecomerce.3wphvcu.mongodb.net/?retryWrites=true&w=majority`;
     try {
-        await mongoose.connect(URL)
+        await mongoose.connect(`mongodb://127.0.0.1:27017/heliverse`, { 
+         serverSelectionTimeoutMS: 5000 // Set to a higher value if needed
+     })
         console.log("database connected")
      }
      catch (error){
@@ -25,30 +27,30 @@ main()
 const PORT=process.env.PORT || 3000
 
 
-// let userSchema=new mongoose.Schema({
-//    id:mongoose.Schema.Types.ObjectId,
-//    first_name:'string',
-//    last_name:"String",
-//    email:"String",
-//    gender:"String",
-//    avatar:"String",
-//    domain:"String",
-//    available:"String"
-// })
-// const User = mongoose.model('User', userSchema);
+let userSchema=new mongoose.Schema({
+   id:mongoose.Schema.Types.ObjectId,
+   first_name:'string',
+   last_name:"String",
+   email:"String",
+   gender:"String",
+   avatar:"String",
+   domain:"String",
+   available:"String"
+})
+const User = mongoose.model('User', userSchema);
 
-app.get('/',(req,res)=>{
+app.get('/',async(req,res)=>{
    
    try {
         // Fetch users
-      //   const users = await User.find();
+        const users = await User.find();
 
         // Log execution statistics
-      //   const explainResult = await User.find({}).explain('executionStats');
-      //   console.log('Execution Stats:', explainResult.executionStats);
+        const explainResult = await User.find({}).explain('executionStats');
+        console.log('Execution Stats:', explainResult.executionStats);
 
         // Send users as JSON response
-      //   res.json(users);
+        res.json(users);
       res.send("ok connected")
     } catch (error) {
       console.error('Error fetching users:', error);
